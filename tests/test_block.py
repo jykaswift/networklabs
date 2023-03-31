f6a0e2f03ec0cc980db91ea02b69540c1d3ef36d
Eugene Borisov <jykaswift@mail.ru>
2023-03-31 20:50:31 +0300

    Issue-#4: create tests

---
+++ tests/test_block.py
@@ -0,0 +1,16 @@
+from src.block import Block
+import pytest
+
+
+@pytest.mark.parametrize('change_by', [1, 2, 3])
+def test_generate_hash(change_by):
+    for i in range(50):
+        block = Block(1, 'prev_hash', 1, 'hash')
+        block.hash_block(change_by)
+        assert block.hash[-4:] == "0000"
+
+
+def test_generate_data():
+    for i in range(100):
+        block = Block(1, 'prev_hash', 1, 'hash')
+        assert len(block.data) == 256
